3b08679aac0ec3dd9eb8a1c284eb96fce70fe0b1
nvxden <clever411@yandex.ru>
2021-07-18 01:42:04 +0300

    shader v1.1: Added switch between gradient and blinking

--- shaders/shader/frag.glsl
+++ shaders/shader/frag.glsl
@@ -5,6 +5,7 @@ out vec4 frag_color;
 in vec3 vertex_color;
 in vec2 frag_position;
 
+uniform int gradient;
 uniform vec4 out_color;
 uniform vec4 bottom_color;
 
@@ -14,7 +15,9 @@ uniform vec4 bottom_color;
 
 void main()
 {
-	if (frag_position.y < 0.0f)
+	if (gradient == 1)
+		frag_color = vec4(vertex_color, 1.0);
+	else if (frag_position.y < 0.0f)
 		frag_color = bottom_color;
 	else
 		frag_color = out_color;
--- shaders/src/mainloop.cpp
+++ shaders/src/mainloop.cpp
@@ -14,6 +14,7 @@ void main_loop()
 	int out_color_location = g_shader.location("out_color");
 	g_shader.use();
 	g_shader.uniform("bottom_color", EXTRC(LIGHT_RED));
+	g_shader.uniform("gradient",     0);
 
 	while (!glfwWindowShouldClose(g_window))
 	{
@@ -27,7 +28,6 @@ void main_loop()
 
 		g_shader.uniform(out_color_location, 0.0f, color, 0.0f, 1.0f);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawArrays(GL_TRIANGLES, 3, 3);
 
 		glfwSwapBuffers(g_window);
 	}
@@ -37,6 +37,12 @@ void handle_input()
 {
 	if (glfwGetKey(g_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(g_window, true);
+
+	if (glfwGetKey(g_window, GLFW_KEY_C) == GLFW_PRESS)
+		g_shader.uniform("gradient", (int)0);
+
+	if (glfwGetKey(g_window, GLFW_KEY_G) == GLFW_PRESS)
+		g_shader.uniform("gradient", (int)1);
 }
 
 
